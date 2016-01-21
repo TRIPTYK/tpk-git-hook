3b8babfe16d7f8a9e4d2c7c9f07145e132df02d7
Gilles BERTRAND <gilles@triptyk.eu>
2016-01-21 11:43:56 +0100

    update bash directions

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,26 @@ var router = express.Router();
 var spawn = require('child_process').spawn;
 /* GET home page. */
 router.post('/', function(req, res) {
-    var commitMsg=req.body.head_commit.message;
-    if(commitMsg === 'update_qual'){
+    var commitMsg = req.body.head_commit.message;
+    if (commitMsg === 'update_qual') {
         console.log('allez on update la qual');
-        spawn(process.cwd()+'bash/hook.sh');
-    } 
-    res.json({status:'update executed'});
+        var dl = spawn(process.cwd() + '/bash/hook.sh');
+        dl.stdout.on('data', function(data) {
+            console.log(data.toString());
+        });
+        dl.stdout.on('end', function(data) {
+            console.log(' downloaded finished ');
+        });
+        // when the spawn child process exits, check if there were any errors and close the writeable stream
+        dl.on('exit', function(code) {
+            if (code != 0) {
+                console.log('Failed: ' + code);
+            }
+        });
+    }
+    res.json({
+        status: 'update executed'
+    });
 });
 
 module.exports = router;
