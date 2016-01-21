033d8a86644602b9d77f8b6ecfb19012590cc026
Gilles BERTRAND <gilles@triptyk.eu>
2016-01-21 11:19:59 +0100

    add bash execution

---
+++ bash/hook.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# First, get the zip file
+cd /var/www/import && wget -O projectmaster.zip -q https://github.com/maxgosset/dockerhook/archive/master.zip
+
+# Second, unzip it, if the zip file exists
+# if [ -f /var/www/import/projectmaster.zip ]; then
+#     # Unzip the zip file
+#     unzip -q /var/www/import/projectmaster.zip
+# 
+#     # Delete zip file
+#     rm /var/www/import/projectmaster.zip
+# 
+#     # Rename project directory to desired name
+#     mv Project-master hook
+# 
+#     # Delete current directory
+#     rm -rf /var/www/somesite.com
+# 
+#     # Replace with new files
+#     mv somesite.com /var/www/
+# 
+#     # Perhaps call any other scripts you need to rebuild assets here
+#     # or set owner/permissions
+#     # or confirm that the old site was replaced correctly
+# fi
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var spawn = require('child_process').spawn;
 /* GET home page. */
 router.post('/', function(req, res) {
     var commitMsg=req.body.head_commit.message;
-    if(commitMsg === "update_qual"){
+    if(commitMsg === 'update_qual'){
         console.log('allez on update la qual');
+        spawn(process.cwd()+'bash/hook.sh');
     } 
     res.json({status:'update executed'});
 });
