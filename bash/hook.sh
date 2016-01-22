880830d3dfe979f881cd317ef322d651728042f8
maxime gosset <maxgosset@MacBook-Pro-de-maxime-2.local>
2016-01-22 15:28:33 +0100

    drupal theme add variable

--- bash/hook.sh
+++ bash/hook.sh
@@ -2,7 +2,7 @@
 
 # First, get the zip file
 
-cd /var/www/import && wget -O $1 -q https://github.com/maxgosset/dockerhook/archive/master.zip
+# cd /var/www/import && wget -O $1 -q https://github.com/maxgosset/dockerhook/archive/master.zip
 
 # # # Second, unzip it, if the zip file exists
 # if [ -f /var/www/import/$1 ]; then
@@ -26,3 +26,41 @@ cd /var/www/import && wget -O $1 -q https://github.com/maxgosset/dockerhook/arch
 #     # or set owner/permissions
 #     # or confirm that the old site was replaced correctly
 # fi
+#!/bin/bash
+
+# First, get the zip file
+
+cd /var/www/import && wget -O $1 -q https://github.com/maxgosset/dockerhook/archive/master.zip
+
+# # Second, unzip it, if the zip file exists
+if [ -f /var/www/import/$1 ]; then
+    # Unzip the zip file
+    unzip -q /var/www/import/$1
+
+    # Delete zip file
+    rm /var/www/import/$1
+
+
+    # Rename project directory to desired name
+    mv $3-master $4
+
+    # Delete current directory
+
+
+    # Replace with new files
+    mv $4 /var/www/$3/$4
+
+    # Perhaps call any other scripts you need to rebuild assets here
+    # or set owner/permissions
+    # or confirm that the old site was replaced correctly
+
+	# modification du fichier default
+
+    sed -i "s/$5/$4/g" /etc/nginx/sites-enabled/default
+    if [ -f "/var/www/$3/$4/last.sql" ]; then
+		echo " add user and database mysql"
+		Q2="CREATE DATABASE IF NOT EXISTS sdfsd;"
+		SQL="${Q2}"
+		mysql -uroot -pMar10KartW11 -e "$SQL"
+    fi
+fi
--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,17 @@ var spawn = require('child_process').spawn;
 var config = require(process.cwd()+'/config/config');
 /* GET home page. */
 router.post('/', function(req, res) {
+    var beforeVersion = req.body.before;
     var commitMsg = req.body.head_commit.message;
     var version = req.body.head_commit.id;
     var name = req.body.repository.name;
     var fileName = name+'_'+version+'.zip';
     if (commitMsg === 'update_qual') {
         console.log('allez on update la qual');
-        var dl = spawn(process.cwd() + '/bash/hook.sh',[fileName,config.destinationFolder,name]);
+        var dl = spawn(process.cwd() + '/bash/hook.sh',[fileName,config.destinationFolder,name,version,beforeVersion]);
  
         dl.stdout.on('end', function(data) {
-            console.log('downloaded finished ');
             console.log(name);
-            console.log(fileName);
             console.log(version); 
         });
         // when the spawn child process exits, check if there were any errors and close the writeable stream
